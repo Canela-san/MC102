eaa01e123c1bdde52276057a92fa5077f97b8e90
Canela-san <sstarksan@gmail.com>
2022-11-01 12:49:48 -0300

    Lab10 update. Lab 2,3,4 small update

--- lab01.py
+++ lab01.py
@@ -4,14 +4,5 @@
 # Nome: Gabriel Canela
 # RA: 243453
 ###################################################
-
 a, b = int(input()), int(input())
-
-print("a =", a)
-print("b =", b)
-print("a + b =", a + b)
-print("a - b =", a - b)
-print("a * b =", a * b)
-print("a ** b =", a ** b)
-print("a // b =", a // b)
-print("a % b =", a % b)
+print("a =", a,"b =", b,"a + b =", a + b,"a - b =", a - b,"a * b =", a * b,"a ** b =", a ** b,"a // b =", a // b,"a % b =", a % b)
--- lab02.py
+++ lab02.py
@@ -3,11 +3,5 @@
 # Laboratório 2 - Rumo a Marte 
 # Nome: Gabriel Canela
 # RA: 243453 ################################################### 
-
-# Vou tentar usar o menor número de linhas possível, isso é apenas para treinar,
-# mesmo que fique menos legível ainda é válido para aprendizado!
-
 D1, V1, T, D2, V2 = int(input()),int(input()),int(input()),int(input()),int(input())
 [print("True") if (((D1 / V1) / 24) - T) < ((D2 / V2) / 24) else print("False")]
-
-#Apenas duas linhas!
