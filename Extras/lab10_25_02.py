3cdee45e15231531ef15634ca5b2a6aec86ce60c
Canela-san <sstarksan@gmail.com>
2025-06-06 19:52:05 -0300

    Adicionado lab extra 11

--- Extras/lab10_25_02.py
+++ Extras/lab10_25_02.py
@@ -8,45 +8,43 @@
 
 # Leitura de dados
 m = int(input())
-matrix = [[0 for _ in range(m)] for _ in range(m)]
+agenda = [[0 for _ in range(m)] for _ in range(m)]
 for i in range(m):
     temp = input()
-    for j in range(m):
-        matrix[i][j] = (int(temp[2*j]))
-
+    agenda[i] = [int(x) for x in temp.split()]
 
 # Processamento de dados
-def verifica_3x3(matrix):
+def verifica_3x3(agenda):
     sum,temp,temp2,diag1,diag2 = 0, 0, 0, 0, 0
     for i in range(3):
-        sum += matrix[0][i]
-        diag1 += matrix[i][i]
-        diag2 += matrix[i][2-i]
+        sum += agenda[0][i]
+        diag1 += agenda[i][i]
+        diag2 += agenda[i][2-i]
     if not (sum == diag1 == diag2):
         return 0
     else:
         for i in range(3):
             temp = 0
             for j in range(3):
-                temp += matrix[i][j]
-                temp2 += matrix[j][i]
+                temp += agenda[i][j]
+                temp2 += agenda[j][i]
             if temp != sum:
                 return 0
     return 1
 
-def gerar_submatrix_3x3(matrix):
-    m = len(matrix)
-    submatrix = []
+def gerar_subagenda_3x3(agenda):
+    m = len(agenda)
+    subagenda = []
     for i in range(m - 2):
         for j in range(m - 2):
-            submatrix.append([ [matrix[i + x][j + y] for y in range(3)] for x in range(3) ])
-    return submatrix
+            subagenda.append([ [agenda[i + x][j + y] for y in range(3)] for x in range(3) ])
+    return subagenda
 
 
 # Saída de dados
-submatrix = gerar_submatrix_3x3(matrix)
+subagenda = gerar_subagenda_3x3(agenda)
 resposta = 0
 for i in range((m-2)**2):
-    resposta += verifica_3x3(submatrix[i])
+    resposta += verifica_3x3(subagenda[i])
 print(f"Número de quadrados mágicos: {resposta}")
 
---
+++ Extras/lab11_25_02.py
@@ -0,0 +1,60 @@
+###################################################
+# MC102 - Algoritmos e Programação de Computadores
+# Laboratório 11 - Organizando a Agenda da Semana
+# Nome:
+# RA:
+###################################################
+
+dias = {
+    "Segunda":0,
+    "Terça":1,
+    "Quarta":2,
+    "Quinta":3,
+    "Sexta":4,
+    }
+
+def main():
+    agenda = [input().split() for _ in range(5)]
+    n = int(input())
+    for i in range(n):
+        comando = input().split()
+        if comando[0] == "Remover":
+            D = dias[comando[1]]
+            I = int(comando[2])
+            F = int(comando[3])
+            for j in range(F - I):
+                agenda[D][I-8+j] = "Livre"
+        if comando[0] == "Adicionar":
+            T = comando[1]
+            H = int(comando[2])
+            hora_livre = Encontra_Horário_Livre(H,agenda)
+            if hora_livre != 0:
+                for i in range(H):
+                    agenda[hora_livre[0]][hora_livre[1]+i] = T
+            else:
+                print(f"Não foi possível alocar a atividade {T}")
+    print_agenda(agenda)
+
+def Encontra_Horário_Livre(H, agenda):
+    contador = 0
+    for i in range(5):
+        contador = 0
+        for j,val in enumerate(agenda[i]):
+            if val == "Livre":
+                contador+=1
+                if contador == H:
+                    return(i,j-H+1)                        
+            else:
+                contador=0
+    return 0
+
+def print_agenda(agenda):
+  print(' '.join([j.ljust(10) for j in ['Horário', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta']]))
+  horario = list(range(8, 19))
+  for i in range(10):
+    saida = [(str(horario[i]) + '-' + str(horario[i+1])).ljust(10)]
+    saida = saida + [agenda[j][i].ljust(10) for j in range(5)]
+    print(' '.join(saida))
+    
+main()
+
